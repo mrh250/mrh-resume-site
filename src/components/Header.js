f5c5b4a8d3a843dbe90a0b0a42dff26288d5624d
Matt Halstead <m.russell.halstead@gmail.com>
2018-11-29 21:14:52 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-diamond"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Dr. Matthew Halstead</h1>
+                <p>A motivated, energetic nuclear engineer and radiation effects in electronics physicist with a passion to get things done for your organization. </p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
